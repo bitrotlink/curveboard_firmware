e8148cb2046c94cb87fed3a20afee895bc499766
Andy Bernard <abg@localhost.localdomain>
2012-04-02 12:35:44 -0700

    Add HID page C to header

--- inter_AVR_protocol.h
+++ inter_AVR_protocol.h
@@ -2,6 +2,7 @@ typedef struct { //Keyboard report.
         uint8_t mod_keys;
         uint8_t reserved;
         uint8_t nonmod_keys[6];
+        uint16_t page_C_key;
         uint8_t report_sequence_number;
         uint8_t try_sequence_number;
         uint8_t fletcher16_checksum[2];
